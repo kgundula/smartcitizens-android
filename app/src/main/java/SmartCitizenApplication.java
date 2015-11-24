baab0446d5efee401900c043ce7f788082489ed0
Khuthadzo Gundula <kgundula@gmail.com>
2015-11-24 14:24:00 +0200

    Added Leak Monitoring Using Leak Canary

---
+++ app/src/main/java/SmartCitizenApplication.java
@@ -0,0 +1,26 @@
+import android.app.Application;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+
+import static android.os.Build.VERSION.SDK_INT;
+import static android.os.Build.VERSION_CODES.GINGERBREAD;
+
+public class SmartCitizenApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        enabledStrictMode();
+        LeakCanary.install(this);
+    }
+
+    private void enabledStrictMode() {
+        if (SDK_INT >= GINGERBREAD) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                    .detectAll() //
+                    .penaltyLog() //
+                    .penaltyDeath() //
+                    .build());
+        }
+    }
+}
